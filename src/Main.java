0c3af4cab12ec4c2e1304720c5d50fdc1c75c885
Kristina Bazaley <atra.nox.ch@gmail.com>
2022-10-27 12:55:37 +0700

    ComplitedTask1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b);
+
+        //код в примере не работл, потому, что в b мы получаем 0,
+        //и пытаемся в с получить результат от деления на 0.
+
+        calc.println.accept(c);
     }
 }
